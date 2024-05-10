b767f99e372d9cb10f6f0cf629c83e625c3a1bec
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-10 16:43:06 -0300

    Arrays

--- Código/script.js
+++ Código/script.js
@@ -1,9 +1,48 @@
-var nome = prompt("Qual seu nome?")
+var area = document.getElementById("area")
 
-var sobrenome = prompt("E qual seu sobrenome?")
+function entrar(){
+    var nome = prompt("Digite seu nome")
 
-console.log("Olá " + nome+" "+sobrenome)
 
-document.write("<h1> Bem vindo, " + nome + "</h1>")
+    if (nome === null || nome === '') {
+        alert("Você não inseriu um nome válido")
+        area.innerHTML = "Clique no Botão para Acessar: "
+    } else{
+        area.innerHTML = "Bem-Vindo " + nome + " "
 
-document.write("<img src='https://sujeitoprogramador.com/steve.png' alt'Foto do Steve Jobs'")
+        let botaoSair = document.createElement("button")
+        botaoSair.innerText = "Sair da conta"
+        botaoSair.onclick = sair
+
+        area.appendChild(botaoSair)
+    }
+}
+
+function sair(){
+    alert("Até mais!")
+    area.innerHTML = "Clique no Botão para Acessar: "
+}
+
+function mediaAluno(nota1, nota2){
+    var media = (nota1+nota2)/2
+    if((nota1 || nota2)>10 || (nota1 || nota2)<0){
+        console.log("NOTAS INVÁLIDAS INSERIDAS")
+        return
+    }
+
+    if (media >= 7 && media <=10) {
+        console.log("Aluno aprovado com a média: " + media)
+    } else if (media <7 && media >=4) {
+        console.log("Aluno em recuperação com a média: " + media)
+    } else if (media >=0 && media <4) {
+        console.log("Aluno reprovado com a média: " + media)
+    } else {
+        console.log("Erro identificado no cálculo da média.\nDEBUG: notas inseridas = " + nota1 + " e " + nota2)
+    }
+}
+
+function aluno(nome, curso){
+    var mensagem = "Bem vindo " + nome + " ao curso de " + curso
+    
+    console.log(mensagem)
+}
---
+++ Código/utilidades.js
@@ -0,0 +1,35 @@
+/*
+
+
+DOCUMENTO DESTINADO UNICA E EXCLUSIVAMENTE PARA SALVAR TUDO QUE EU APRENDER
+
+
+*/
+
+function t (){} // Utiizado par afunções
+
+var a = "Declaração de variável, não são tipadas"
+a = 12 // variáveis podem mudar de valor
+
+console.log("Serve pra imprimir algo em console")
+
+document.getElementById(id) // pega um elemento do html para ser editado por seu ID, geralmante usado assim:
+
+var variavel = getElementById(pastel) 
+
+//arrays: 
+var lista = [1,165,"dada","pastel"]
+
+lista.indexOf("pastel") // retorna a posição de pastel na lista
+
+lista[2] // padrão né.
+
+lista.indexOf("nada") // retorna -1 sempre que não existir na lista.
+
+lista[posicao] = novoValor //altera o valor de uma posição
+
+lista.push(valor) // adiciona um valor à lista
+
+lista.shift(index) // tira da lista
+
+lista.join(";") // muda o separador da lista
