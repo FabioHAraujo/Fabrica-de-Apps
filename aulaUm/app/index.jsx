c21d59bc1bf737f2b782cd543d62162eab5fd1d1
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-15 15:37:04 -0300

    View, Text, Image, Style e Functions dentro do React Native

--- aulaUm/app/index.jsx
+++ aulaUm/app/index.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 function Index(){
+  let nome = "Fábio Henrique"
+  let img = "https://sujeitoprogramador.com/reactlogo.png"
   return(
     <View>
       <Text>Olá Mundo!!!!!!</Text>
       <Text style={{ color: 'blue', fontSize: 25 }}>Meu Primeiro App</Text>
+      <Text style={{ fontSize: 18, color: 'green' }}>
+        Sujeito Programador
+      </Text>
+
+      <Logo largura={350} altura={350} fulano="Lucas Oliveira" />
+
+      <Text>{nome}</Text>
     </View>
   );
 };
 
 export default Index;
+
+function Logo(props) {
+  let img = "https://sujeitoprogramador.com/reactlogo.png"
+  
+  return(
+    <View>
+      <Image source={{ uri: img }} style={{ width: props.largura, height: props.altura}} />
+      <Text>{props.fulano}</Text>
+    </View>
+  )
+
+}
