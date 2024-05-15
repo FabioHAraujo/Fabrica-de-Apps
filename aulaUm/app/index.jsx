b1ce4c7659546928bbaded02c449db25f2038c02
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-15 16:04:14 -0300

    UserState

--- aulaUm/app/index.jsx
+++ aulaUm/app/index.jsx
@@ -1,34 +1,26 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
 
 function Index(){
-  let nome = "Fábio Henrique"
-  let img = "https://sujeitoprogramador.com/reactlogo.png"
+  const [nome, setNome] = useState("Paulo");
+  const [idade, setIdade] = useState(20);
+
+  function entrar(nome, idade){
+    setNome(nome)
+    setIdade(idade)
+  }
+
   return(
-    <View>
-      <Text>Olá Mundo!!!!!!</Text>
-      <Text style={{ color: 'blue', fontSize: 25 }}>Meu Primeiro App</Text>
-      <Text style={{ fontSize: 18, color: 'green' }}>
-        Sujeito Programador
-      </Text>
-
-      <Logo largura={350} altura={350} fulano="Lucas Oliveira" />
-
-      <Text>{nome}</Text>
+    <View style = {{ marginTop: 25 }}>
+      <Button title="Mudar nome" onPress={()=>{entrar('Lucas Oliveira', 33)}}/>
+      <Text style={{fontSize:19}} >{nome}</Text>
+      <Text style={{fontSize:17}} >{idade}</Text>
     </View>
   );
-};
+}
 
 export default Index;
 
-function Logo(props) {
-  let img = "https://sujeitoprogramador.com/reactlogo.png"
+// function setNome(){
   
-  return(
-    <View>
-      <Image source={{ uri: img }} style={{ width: props.largura, height: props.altura}} />
-      <Text>{props.fulano}</Text>
-    </View>
-  )
-
-}
+// }
