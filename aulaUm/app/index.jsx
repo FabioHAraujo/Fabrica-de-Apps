43e9a7ca071e5a0f90b3140b46d5e4a9eabb0dbb
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-15 15:00:18 -0300

    Criando o app da primeira aula de React Native

---
+++ aulaUm/app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Index(){
+  return(
+    <View>
+      <Text>Olá Mundo!!!!!!</Text>
+      <Text style={{ color: 'blue', fontSize: 25 }}>Meu Primeiro App</Text>
+    </View>
+  );
+};
+
+export default Index;
