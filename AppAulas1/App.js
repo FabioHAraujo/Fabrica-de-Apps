3fb684fa950c12417a41b8c52713c6e3bdf1dc3e
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-16 11:06:57 -0300

    Flex - Posicionamento e organização.

--- AppAulas1/App.js
+++ AppAulas1/App.js
@@ -3,10 +3,15 @@ import { View, Text, StyleSheet } from 'react-native';
 
 function Index(){
   return(
-    <View style={{flex: 1, backgroundColor: '#a9a9a9'}}>
-      <View style={{height:65, backgroundColor: '#a9a9a9' }}></View>
-      <View style={{flex:1, backgroundColor: '#ddd' }}></View>
-      <View style={{height:65, backgroundColor: '#a9a9a9' }}></View>
+    <View style={{
+      flex: 1, 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      backgroundColor: 'lightblue'}}>
+      <View style={{height:50, width:50, backgroundColor: 'black' }}></View>
+      <View style={{height:50, width:50, backgroundColor: 'gray' }}></View>
+      <View style={{height:50, width:50, backgroundColor: 'white' }}></View>
     </View>
   );
 }
