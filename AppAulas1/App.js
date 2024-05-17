5fa0f2e7df1af9197eac2857b21196274edcd511
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-17 11:21:21 -0300

    ScrollView Horizontal e Vertical

--- AppAulas1/App.js
+++ AppAulas1/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StatusBar, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, StatusBar, Text, StyleSheet, ScrollView } from 'react-native';
 
 let timer = null
 let ss = 0
@@ -7,75 +7,16 @@ let mm = 0
 let hh = 0
 
 function Index(){
-
-  const [tempo, setTempo] = useState('00:00:00')
-  const [botao, setBotao] = useState('INICIAR')
-  const [ultimo, setUltimo] = useState(null)
-
-  function iniciar(){
-    if(timer !== null){
-      clearInterval(timer)
-      timer = null
-      setBotao('RETOMAR')
-    } else {
-      timer= setInterval(()=>{
-        ss++
-        if(ss==60) {
-          ss = 0;
-          mm ++;
-        }
-        if(mm==60){
-          mm = 0
-          hh++
-        } 
-
-        let format =
-        (hh < 10 ? '0' + hh : hh) + ':'
-        + (mm < 10 ? '0' + mm : mm) + ':'
-        + (ss < 10 ? '0' + ss : ss)
-
-        setTempo(format)
-      }, 1000)
-      setBotao('PARAR')
-    }
-  }
-
-  function limpar(){
-    if(timer!==null){
-      clearInterval(timer)
-      timer = null
-    }
-    setBotao('INICIAR')
-    setUltimo(tempo)
-    ss = 0
-    mm = 0
-    hh = 0
-    setTempo('00:00:00')
-  }
-
   return(
     <View style = {styles.container}>
-      <StatusBar backgroundColor={'#00aeef'} barStyle={'dark-content'}/>
-      <View style={styles.timerArea}>
-        <Image 
-          source ={require('./images/crono.png')}
-          style={styles.img}
-        />
-        <Text style={styles.timer}> {tempo} </Text>
-      </View>
-      <View style={styles.btnArea}>
-        <TouchableOpacity style={styles.btn} onPress={iniciar}>
-          <Text style={styles.btnTexto}>{botao}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={limpar}>
-          <Text style={styles.btnTexto}>LIMPAR</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.areaUltimo}>
-        <Text style={styles.ultimoTexto}>
-          {ultimo ? 'Ultimo Tempo: ' + ultimo : ''}
-        </Text>
-      </View>
+      <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'}/>
+      <ScrollView showsHorizontalScrollIndicator={false} horizontal={true}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+        <View style={styles.box2}></View>
+      </ScrollView>
     </View>
   );
 }
@@ -84,54 +25,27 @@ function Index(){
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
-    backgroundColor: '#00aeef',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex:1
   },
-  img:{
-    position: 'absolute',
+  box1:{
+    height: 250,
+    width: 150,
+    backgroundColor: 'red'
   },
-  timerArea:{
-    justifyContent:'center',
-    alignItems: 'center',
-    margin:100
+  box2:{
+    height: 250,
+    width: 150,
+    backgroundColor: 'green'
   },
-  btnArea:{
-    flexDirection: 'row',
-    padding: 30,
+  box3:{
+    height: 250,
+    width: 150,
+    backgroundColor: 'yellow'
   },
-  timer:{
-    marginTop: 50,
-    textAlign: 'center',
-    fontSize: 45,
-    fontWeight: 'bold',
-    color: '#FFF'
-  },
-  btn:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    height: 40,
-    margin: 17,
-    borderRadius: 9
-  },
-  btnTexto:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#00aeef'
-  }, 
-  areaUltimo:{
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  ultimoTexto:{
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: 'white',
-    fontStyle: 'italic'
+  box4:{
+    height: 250,
+    width: 150,
+    backgroundColor: 'blue'
   }
 })
 
