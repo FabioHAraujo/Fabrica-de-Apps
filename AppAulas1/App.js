8214a79d02d38529633ef79ce7e1b6109effc9ce
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-17 15:14:53 -0300

    Desafio Usuário
    Um desafio simples, mas fiz o possível para ir um pouco além.

--- AppAulas1/App.js
+++ AppAulas1/App.js
@@ -1,38 +1,47 @@
-import React, { useState } from 'react';
-import { View, StatusBar, StyleSheet, FlatList, Text } from 'react-native';
-import Pessoa from './src/Pessoa'
+import React from "react";
+import { View, StatusBar, StyleSheet, Text } from "react-native";
+import Usuario from "./src/Usuario/usuario";
 
+const usuarios = [
+  { id: "1", nome: "Fábio Araújo", cargo: "Desenvolvedor" },
+  { id: "2", nome: "Henrique Silva", cargo: "Gerente de Projetos" },
+  { id: "3", nome: "Maria Santos", cargo: "Analista de Sistemas" },
+  { id: "4", nome: "João Oliveira", cargo: "Designer" },
+  { id: "5", nome: "Ana Souza", cargo: "Engenheira de Software" },
+  { id: "6", nome: "Carlos Santos", cargo: "Administrador de Redes" },
+  { id: "7", nome: "Luciana Pereira", cargo: "Suporte Técnico" },
+  { id: "8", nome: "Roberto Lima", cargo: "Gerente de TI" },
+];
 
-function Index(){
+function Index() {
+  const usuario = usuarios.find((user) => user.id === "5");
 
-  const [feed, setFeed] = useState([
-    {_id: '1', nome: 'Fábio', idade: 23, email: 'fabio@example.com'},
-    {_id: '2', nome: 'Henrique', idade: 84, email: 'henrique@example.com'},
-    {_id: '3', nome: 'Maria', idade: 45, email: 'maria.silva@example.com'},
-    {_id: '4', nome: 'João', idade: 30, email: 'joao.oliveira@example.com'},
-    {_id: '5', nome: 'Ana', idade: 28, email: 'ana.souza@example.com'},
-    {_id: '6', nome: 'Carlos', idade: 50, email: 'carlos.santos@example.com'}
-  ])
-
-  return(
-    <View style = {styles.container}>
-      <StatusBar backgroundColor={'#fff'} barStyle={'dark-content'}/>
-      <FlatList 
-        data={feed}
-        keyExtractor={(item) => item._id} //Necessário somente caso o id não venha como "id"
-        renderItem={({item})=><Pessoa data={item}/>}
-        showsVerticalScrollIndicator={false}
-      />
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor={"#fff"} barStyle={"dark-content"} />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Seja Bem-Vindo</Text>
+      </View>
+      <Usuario data={usuario}/>
     </View>
   );
 }
 
-
-
 const styles = StyleSheet.create({
-  container:{
-    flex:1
-  }
-})
+  container: {
+    flex: 1,
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ddd",
+    height: 60,
+  },
+  headerText: {
+    color: "#000",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
 
 export default Index;
