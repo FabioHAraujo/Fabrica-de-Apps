847d264e1aa0b33a572d72ea4e137b56a4748545
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-17 16:28:45 -0300

    Slider

--- AppAulas1/App.js
+++ AppAulas1/App.js
@@ -1,36 +1,27 @@
 import React, { useState } from "react";
 import { View, StatusBar, StyleSheet, Text } from "react-native";
-import { Picker } from "@react-native-picker/picker";
+import Slider from '@react-native-community/slider';
 
 function Index() {
-  const [carro, setCarro] = useState(0)
-  const [listaCarros, setlistaCarros] = useState([
-    {key: 1, nome: 'Golf 1.6', valor: 62000},
-    {key: 2, nome: 'Jetta 2.0', valor: 75000},
-    {key: 3, nome: 'Passat 2.0', valor: 90000},
-    {key: 4, nome: 'Tiguan 2.0', valor: 100000},
-    {key: 5, nome: 'T-Cross 1.4', valor: 80000},
-    {key: 6, nome: 'Amarok 3.0', valor: 120000},
-    {key: 7, nome: 'Maverick V8', valor: 400000}
-  ])
+  const [valor, setValor] = useState(50)
 
-  let listaCarrosItem = listaCarros.map((v, k) => {
-    return <Picker.Item key={k} value={k} label={v.nome}/>
-  })
 
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={"#fff"} barStyle={"dark-content"} />
-      <Picker
-        selectedValue={carro}
-        onValueChange={(itemValue, itemIndex) => setCarro(itemValue)}
-      >
-        {listaCarrosItem}
-      </Picker>
+      <Text style={styles.texto}>Selecione seu peso:</Text>
+      <Slider 
+        maximumValue={0}
+        maximumValue={100}
+        value={valor}
+        onValueChange={(valorSelecionado)=>setValor(valorSelecionado)}
+        minimumTrackTintColor="blue"
+        maximumTrackTintColor="red"
+        thumbTintColor="purple"
+      />
 
-      <Text style={styles.listaCarros}>Carro: {listaCarros[carro].nome}</Text>
-      <Text style={styles.listaCarros}>Valor: {listaCarros[carro].valor}</Text>
-
+      <Text style={styles.valor}>Você tem: {valor.toFixed(0)}kg</Text>
+      
     </View>
   );
 }
@@ -38,10 +29,15 @@ function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 35
   },
-  carro:{
-    marginTop: 15,
-    fontSize: 25
+  valor:{
+    textAlign: 'center'
+  },
+  texto:{
+    textAlign: 'center',
+    color: 'cian',
+    fontSize: 22,
   }
 });
 
