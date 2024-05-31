8a322d6b6e31b2013702703e50054c4926d8a556
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-05-31 15:33:47 -0300

    Tela de Loading do aplicativo

--- Avançando com React Native/InfoFilmes/App.tsx
+++ Avançando com React Native/InfoFilmes/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList, StatusBar } from "react-native";
+import { StyleSheet, Text, View, FlatList, StatusBar, ActivityIndicator } from "react-native";
 
 import Filmes from "./src/Filmes";
 
@@ -7,26 +7,36 @@ import api from "./src/services/api";
 
 export default function App() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
       const response = await api.get('r-api/?api=filmes')
       // console.log(response.data)
       setFilmes(response.data)
+      setLoading(false)
     }
     loadFilmes();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor={'#D5DBDB'} barStyle={'dark-content'}/>
-      <FlatList 
-        showsVerticalScrollIndicator={false}
-        data={filmes}
-        renderItem={({item}) => <Filmes data={item} />}
-      />
-    </View>
-  )
+  if(loading){
+    return(
+      <View style={styles.loading}>
+        <ActivityIndicator color="#121212" size={45} />
+      </View>
+    )
+  } else{
+      return (
+        <View style={styles.container}>
+          <StatusBar backgroundColor={'#D5DBDB'} barStyle={'dark-content'}/>
+          <FlatList 
+            showsVerticalScrollIndicator={false}
+            data={filmes}
+            renderItem={({item}) => <Filmes data={item} />}
+          />
+        </View>
+      )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -34,4 +44,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#D5DBDB",
   },
+  loading:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
