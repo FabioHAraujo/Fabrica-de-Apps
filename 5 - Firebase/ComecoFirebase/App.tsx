5bbcea17a86dd5548665de46dc366545d8120b1f
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-06-03 11:07:13 -0300

    EcoReturn - Iniciando Projeto | Tela de login

--- 5 - Firebase/ComecoFirebase/App.tsx
+++ 5 - Firebase/ComecoFirebase/App.tsx
@@ -2,29 +2,31 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { database } from "./src/firebase/connection";
-import { ref, get } from "firebase/database";
+import { ref, set, get, update } from "firebase/database";
 
 const App = () => {
   const [nome, setNome] = useState<string | null>("Carregando...");
   const [idade, setIdade] = useState<string | null>("Carregando...");
 
   useEffect(() => {
-    const fetchNome = async () => {
-      const nomeRef = ref(database, "usuarios/1");
-      try {
-        const snapshot = await get(nomeRef);
-        if (snapshot.exists()) {
-          setNome(snapshot.val().nome);
-          setIdade(snapshot.val().idade);
-        } else {
-          console.log("Sem dados disponiveis");
-        }
-      } catch (error) {
-        console.error("Erro buscando dados: ", error);
-      }
-    };
+    async function dados(){
+      // await set(ref(database, 'tipo'), 'Cliente')
+      // const resultado = await get(ref(database, 'tipo'))
+      // console.log(resultado)
 
-    fetchNome();
+      // await set(ref(database, 'usuarios/'+4), {
+      //   nome: 'Daniel',
+      //   cargo: 'Skatista Pro'
+      // })
+      await update(ref(database, 'usuarios/'+4), {
+        nome: 'Charles Emanuel',
+        idade: 15
+      })
+
+      const resultado = await get(ref(database, 'usuarios/4'))
+      console.log(resultado)
+    }
+    dados()
   }, []);
 
   return (
